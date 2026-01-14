a6aa3a97fb44a50ff089f6e651434ac0591b8d0e
manojkumar-SDET <killampallimanojkumar@gmail.com>
2026-01-14 20:40:21 +0530

    second assignment

---
+++ javascript-training/assignment/assignment2.ts
@@ -0,0 +1,113 @@
+
+//Assignment1
+
+//1.Temperature of a city in degrees Celsius: 25.5
+export let cityTemparature : number = 25.5;
+console.log("city Temparature:", cityTemparature);
+
+
+//2.Whether a customer has placed an order: true or false
+let customerOrdered:boolean = true;
+console.log("customer Ordered:", customerOrdered);
+
+//3.Person's phone number: "123-456-7890"
+let perPhno:string="123-456-7890";
+console.log("Person Phpne no:", perPhno);
+
+//4.Amount of money in a customer's bank account: 1000.50
+let bankBal:number=1000.50;
+console.log("Bank Balance:", bankBal);
+
+//5.Person's email address: "john.doe@example.com"
+let perEmail:string="john.doe@example.com";
+console.log("Email:", perEmail);
+
+//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let perCoord:number[]=[37.7749, -122.4194];
+console.log("Coordinates:", perCoord[0],perCoord[1]);
+
+//7.Person's marital status: true or false
+let perMartialStatus:boolean=true;
+console.log("MaritalStatus:", perMartialStatus);
+
+//8.Person's occupation: 
+let perOccup:string="Software Engineer";
+console.log("Occupation:", perOccup);
+
+//9Person's favourite colour: "Blue"
+let perFavClr:string="Blue";
+console.log("Favourite Color:", perFavClr);
+
+//10.Current year: 2026
+let currentYear:number=2026;
+console.log("Current Year:", currentYear);
+
+//11.Number of followers on a social media platform: 1,000,000
+let followers:string="1000000";
+console.log("Followers:", followers);
+
+//12. Rating of a movie: 7.5
+let mveRating:number=7.5;
+console.log("Movie Rating:", mveRating);
+
+//13.Person's blood type: 'A'
+let bloodGrp:string="A";
+console.log("Blood Group:", bloodGrp);
+
+//14.Title of a book: "To Kill a Mockingbird"
+let bookTitle:string="To Kill a Mockingbird";
+console.log("Book Title:", bookTitle);
+
+//15.Number of employees in a company: 500
+let numberofEmp:number=500;
+console.log("No of Employees:", numberofEmp);
+
+//16.Time of an event: 2:30 PM
+let eventTime:string="2.30 PM";
+console.log("Event Time:", eventTime);
+
+//17.Name of a country: 
+let countryName:string= "United States";
+console.log("Country Name:", countryName);
+
+//18.Person's eye color: "Brown"
+let eyeClr:string="Brown";
+console.log("Eye Colour:", eyeClr);
+
+//19.Person's birthplace: "New York City"
+let birthplace:string="New York City";
+console.log("Birth Place:", birthplace);
+
+//20.Distance between two cities: 200.5
+let distance:number=200.5;
+console.log("Distance between two Cities:", distance);
+
+ 
+/*
+let personDetails = {
+    cityTemperature: 25.5,
+    customerOrdered: true,
+    phoneNumber: "123-456-7890",
+    bankBalance: 1000.50,
+    email: "john.doe@example.com",
+    coordinates: [37.7749, -122.4194],
+    maritalStatus: true,
+    occupation: "Software Engineer",
+    favouriteColor: "Blue",
+    currentYear: 2026,
+    followers: 1000000,
+    movieRating: 7.5,
+    bloodGroup: "A",
+    bookTitle: "To Kill a Mockingbird",
+    numberOfEmployees: 500,
+    eventTime: "2:30 PM",
+    countryName: "United States",
+    eyeColor: "Brown",
+    birthPlace: "New York City",
+    distanceBetweenCities: 200.5
+};
+
+console.log(personDetails);
+
+*/
+
